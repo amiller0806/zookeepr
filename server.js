b2d6c9693b4e33d17df1a5f0d5674fc9d182694e
amiller0806 <ariel.miller555@gmail.com>
2022-12-02 15:22:19 -0500

    Deploy to Heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 
 const {animals} = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
@@ -67,8 +68,8 @@ app.get('/api/animals', (req, res) => {
 });
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 
 });
 // PORT: Likewise, if you have the address for a 
